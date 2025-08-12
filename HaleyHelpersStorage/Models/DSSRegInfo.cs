837bdf691da2e57107f94a014a81b766d0d8b1df
Quetzal <rmsmech@outlook.com>
2025-08-13 00:15:47 +0400

    errors fixed

---
+++ HaleyHelpersStorage/Models/DSSRegInfo.cs
@@ -0,0 +1,27 @@
+﻿using Haley.Abstractions;
+using Haley.Utils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Haley.Enums;
+using System.Linq;
+using System.Text.Json.Serialization;
+
+namespace Haley.Models {
+    public class DSSRegInfo {
+        [JsonPropertyName("client")]
+        public string Client { get; set; }
+        [JsonPropertyName("module")]
+        public string Module { get; set; }
+        [JsonPropertyName("password")]
+        public string Password { get; set; }
+        [JsonPropertyName("space")]
+        public string Workspace { get; set; }
+        [JsonPropertyName("control")]
+        public OSSControlMode Control { get; set; }
+        [JsonPropertyName("parse")]
+        public OSSParseMode Parse { get; set; }
+        [JsonPropertyName("virtual")]
+        public bool IsVirtual { get; set; }
+    }
+}
